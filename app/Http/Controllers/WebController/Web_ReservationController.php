d4027dcabbacced20e1699bf9736a9fd57430f2c
Mark Joseph Castelo <markjoseph475@gmail.com>
2019-01-03 08:28:30 +0800

    fix roomtypes in reservation

--- app/Http/Controllers/WebController/Web_ReservationController.php
+++ app/Http/Controllers/WebController/Web_ReservationController.php
@@ -12,17 +12,11 @@ class Web_ReservationController extends Controller
 {
     public function index(){
         $variables = $this->getSystemVariables();
-
-        // $roomTypes = RoomTypeModel::leftJoin('room_images', 'roomtypes.id', '=', 'room_images.roomtype_id')
-        // ->orderby('roomtypes.created_at', 'desc')
-        // ->limit(3)
-        // ->get();
         $roomTypes = DB::select('select A.id, A.type, A.description,A.maxAdult,
-        A.maxChildren,(select filename from room_images where roomtype_id = A.id
-        order by created_at desc limit 1) as img 
-        from roomtypes A ORDER by A.created_at DESC limit 3');
-        // print_r($roomTypes); exit;
-
+            A.maxChildren,(select filename from room_images where roomtype_id = A.id
+            order by created_at desc limit 1) as img 
+            from roomtypes A ORDER by A.created_at DESC limit 3');
+        
         $data = [
             'variables' => $variables,
             'roomTypes' => $roomTypes
