cbb43f435451d843b8d3f5f4fd781825461a3bb3
Mark Castelo <mark.castello@gameloft.com>
2019-01-10 16:52:17 +0800

    asdad

--- app/Http/Controllers/WebController/ChooseRoomController.php
+++ app/Http/Controllers/WebController/ChooseRoomController.php
@@ -18,6 +18,7 @@ class ChooseRoomController extends Controller
             A.maxChildren,(select filename from room_images where roomtype_id = A.id
             order by created_at desc limit 1) as img 
             from roomtypes A ORDER by A.created_at DESC');
+            
         $room_images = RoomImagesModel::all();
 
         $data = [
--- resources/views/pages/web/ChooseRoomType/index.blade.php
+++ resources/views/pages/web/ChooseRoomType/index.blade.php
@@ -8,7 +8,7 @@
                     <ul class="collapsible mt40">
                         @foreach($roomtypes as $roomtype)
                             <li class="chooseRoomsLi">
-                                <div class="collapsible-header">
+                                <div class="collapsible-header roomType-header">
                                     <i class="material-icons">hotel</i>
                                     {{ $roomtype->type }}
                                     <span class="new badge blue lighten-1 tooltipped" data-tooltip="Available" data-badge-caption="">4
@@ -18,14 +18,14 @@
                                     <div class="row mb0">
                                         <div class="col s12 m6">
                                             <span>
-                                                <img class="responsive-img materialboxed z-depth-1 mb20" src="<?php echo ($roomtype->img != "") ? '/images/' . $roomtype->img : "" ?>" alt="">
+                                                <img style="height:270px; width:100%;" class="responsive-img materialboxed z-depth-1 mb20" src="<?php echo ($roomtype->img != "") ? '/images/' . $roomtype->img : "" ?>" alt="">
                                             </span>
                                             <div class="row mb0">
                                                 @foreach($room_images as $image)
                                                     @if($image->roomtype_id == $roomtype->id)
                                                         <div class="col s4 m4">
                                                             <div class="image-container" style="height:80px;">
-                                                                <img style="height:80px; width:100%;" class="responsive-img materialboxed z-depth-1" 
+                                                                <img style="height:80px; width:100%;" class="responsive-img materialboxed z-depth-1 img-list" 
                                                                 src="<?php echo ($image->filename != "") ? '/images/uploads/' . $image->filename : "" ?>" alt="">
                                                             </div>
                                                         </div>
@@ -35,7 +35,7 @@
                                         </div>
                                         <div class="col s12 m6">
                                             <h5>{{ isset($roomtype->description) ? "Room Info" : "" }}</h5>
-                                            <div>
+                                            <div class="room_desc">
                                                 <?php echo isset($roomtype->description) ? $roomtype->description : ""; ?>
                                             </div>
                                         </div>
