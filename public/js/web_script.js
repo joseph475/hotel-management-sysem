7ed5066aeb6677804180183b24657fd59252214c
joseph475 <markjoseph475@gmail.com>
2022-03-15 19:56:41 +0800

    fix bug

--- public/js/web_script.js
+++ public/js/web_script.js
@@ -1,29 +1,29 @@
 var curpage = 1;
 
 $(document).ready(function ($) {
-    jconfirm.defaults = { content : '', theme: 'dark', boxWidth: '35%', useBootstrap: false };
-    if ($(".roomTypeDropDown").length){ loadRoomTypesDropdown() }
+    jconfirm.defaults = { content: '', theme: 'dark', boxWidth: '35%', useBootstrap: false };
+    if ($(".roomTypeDropDown").length) { loadRoomTypesDropdown() }
 });
 
-function checkifValidated(checkifvalid){
-    for (i = 0 ; i < checkifvalid.length ; i++) {
-        if(!$(checkifvalid[i]).val()){
+function checkifValidated(checkifvalid) {
+    for (i = 0; i < checkifvalid.length; i++) {
+        if (!$(checkifvalid[i]).val()) {
             return false;
         }
     }
     return true;
 }
 
-function checkValid(checkifvalid){
-    for (i = 0 ; i < checkifvalid.length ; i++) {
-        if(!$(checkifvalid[i]).hasClass('valid')){
+function checkValid(checkifvalid) {
+    for (i = 0; i < checkifvalid.length; i++) {
+        if (!$(checkifvalid[i]).hasClass('valid')) {
             return false;
         }
     }
     return true;
 }
 
-function displayMessage(title, content){
+function displayMessage(title, content) {
     $.alert({
         title: title,
         content: content,
