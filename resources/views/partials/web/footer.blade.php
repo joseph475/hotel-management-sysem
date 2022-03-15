eaa272165d87eef4407ab95f034df73bdaedc278
joseph475 <markjoseph475@gmail.com>
2022-03-15 20:23:41 +0800

    fix bug

--- resources/views/partials/web/footer.blade.php
+++ resources/views/partials/web/footer.blade.php
@@ -8,12 +8,13 @@
 
 {{-- <script type="text/javascript" src="{{ url('/js/jquery-3.3.1.min.js') }}"></script> --}}
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
 
 <script type="text/javascript" src="{{ url('/js/fullpage.js') }}"></script>
 <script type="text/javascript" src="{{ url('/js/scrolloverflow.min.js') }}"></script>
 <script type="text/javascript" src="{{ url('/js/materialize.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script type="text/javascript" src="{{ url('/js/jquery-confirm.js') }}"></script>
+
+{{-- <script type="text/javascript" src="{{ url('/js/jquery-confirm.js') }}"></script> --}}
 <script type="text/javascript" src="{{ url('/js/web_script.js') }}"></script>
 
 
